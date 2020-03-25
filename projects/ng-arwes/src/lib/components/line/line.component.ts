35c96bf0cdbf5aff81254836a420784b77705748
jadertao <jader_tao@qq.com>
2020-03-26 01:54:58 +0800

    feat(line): line component supports theme

--- projects/ng-arwes/src/lib/components/line/line.component.ts
+++ projects/ng-arwes/src/lib/components/line/line.component.ts
@@ -1,33 +1,54 @@
+import { ThemeService } from '../../services/theme.service';
 import { DEFAULT_THEME } from './../../tools/theme';
 import { NgArwesTheme } from './../../types/theme';
 import { NgArwesLayerStatusEnum } from '../../types/theme';
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'arwes-line',
   template: `
-<div class="arwes-line">
-  <div class="arwes-line-body"></div>
-  <div class="arwes-line-left"></div>
-  <div class="arwes-line-right"></div>
-</div>
+    <div class="arwes-line" [style.margin-bottom.px]="theme.margin">
+      <div
+        class="arwes-line-body"
+        [style.border-color]="theme.color[layer].dark"
+        [style.transition]="'all ' + theme.animTime + 'ms ease-out'"
+      ></div>
+      <div
+        class="arwes-line-left"
+        [style.background-color]="theme.color[layer].dark"
+        [style.transition]="
+          'all ' + (theme.animTime / 4) * 3 + 'ms ease-out' + theme.animTime / 4
+        "
+      ></div>
+      <div
+        class="arwes-line-right"
+        [style.background-color]="theme.color[layer].dark"
+        [style.transition]="
+          'all ' + (theme.animTime / 4) * 3 + 'ms ease-out' + theme.animTime / 4
+        "
+      ></div>
+    </div>
   `,
   styleUrls: ['./line.component.less']
 })
-export class LineComponent implements OnInit {
+export class LineComponent implements OnInit, OnDestroy {
+  public theme: NgArwesTheme = DEFAULT_THEME;
+  private sub: Subscription;
   @Input()
   show = true;
 
   @Input()
   layer = NgArwesLayerStatusEnum.Primary;
 
-  @Input()
-  theme = DEFAULT_THEME;
-
-  constructor() { }
+  constructor(public ThemeSvc: ThemeService) { }
 
   ngOnInit(): void {
+    this.sub = this.ThemeSvc.theme.subscribe(theme => {
+      this.theme = theme;
+    });
   }
-
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
