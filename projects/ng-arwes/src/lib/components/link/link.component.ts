d584bff5b6ab4e9abb8b28592fea3a5b8e4e35a1
jadertao <jader_tao@qq.com>
2020-04-01 23:14:39 +0800

    feat(link): change style with renderer2

--- projects/ng-arwes/src/lib/components/link/link.component.ts
+++ projects/ng-arwes/src/lib/components/link/link.component.ts
@@ -4,10 +4,12 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { rgba } from 'polished';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { styleObject2String } from './../../tools/style';
 import { NgArwesTheme } from './../../types/theme.interfaces';
 import { ThemeService } from './../../services/theme.service';
-import { Component, OnDestroy, HostBinding, ViewEncapsulation, HostListener } from '@angular/core';
+import {
+  Component, OnDestroy, HostBinding, ViewEncapsulation, HostListener,
+  Renderer2, ElementRef, ChangeDetectionStrategy
+} from '@angular/core';
 
 const LinkSelector = 'a[arwes-link]';
 
@@ -16,27 +18,27 @@ const LinkSelector = 'a[arwes-link]';
   styleUrls: ['./link.component.less'],
   template: '<ng-content></ng-content>',
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LinkComponent implements OnDestroy {
   public hover = false;
+
   private destroy$ = new Subject<void>();
   private theme: NgArwesTheme = DEFAULT_THEME;
-
-  @HostBinding('style.color') color: SafeStyle;
+  private el: HTMLElement = this.elementRef.nativeElement;
 
   @HostBinding('class.arwes-link') className = true;
-  @HostBinding('style') staticStyle: SafeStyle;
 
   constructor(
     public themeSvc: ThemeService,
-    private sanitizer: DomSanitizer,
+    private renderer: Renderer2,
+    private elementRef: ElementRef,
   ) {
     this.themeSvc.theme$
       .pipe(
         takeUntil(this.destroy$)
       ).subscribe(theme => {
         this.deriveStyleFromTheme(theme);
-        Promise.resolve().then(() => this.deriveColorFromTheme(theme));
       });
   }
 
@@ -49,16 +51,17 @@ export class LinkComponent implements OnDestroy {
     this.deriveColorFromTheme();
   }
 
-  deriveStyleFromTheme(theme: NgArwesTheme = this.theme) {
-    this.staticStyle = this.sanitizer.bypassSecurityTrustStyle(styleObject2String({
-      'text-shadow': `0px 0px ${theme.shadowLength}px ${rgba(theme.color.control.base, theme.alpha)}`,
-      transition: `color ${theme.animTime}ms ease-out`,
-    }));
+  deriveStyleFromTheme(theme: NgArwesTheme = this.theme, withColor: boolean = true) {
     this.theme = theme;
+    this.renderer.setStyle(this.el, 'text-shadow', `0px 0px ${theme.shadowLength}px ${rgba(theme.color.control.base, theme.alpha)}`);
+    this.renderer.setStyle(this.el, 'transition', `color ${theme.animTime}ms ease-out`);
+    if (withColor) {
+      this.renderer.setStyle(this.el, 'color', this.hover ? theme.color.control.light : theme.color.control.base);
+    }
   }
 
   deriveColorFromTheme(theme: NgArwesTheme = this.theme) {
-    this.color = this.sanitizer.bypassSecurityTrustStyle(this.hover ? theme.color.control.light : theme.color.control.base);
+    this.renderer.setStyle(this.el, 'color', this.hover ? theme.color.control.light : theme.color.control.base);
   }
 
   ngOnDestroy(): void {
