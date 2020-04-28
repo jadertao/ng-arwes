4bab4ae0ca55e828bf3bffb317d3f9f804cbd2ad
jadertao <jader_tao@qq.com>
2020-04-28 22:52:09 +0800

    feat(image): init image component

---
+++ projects/ng-arwes/src/lib/components/Image/Image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arwes-Image',
+  templateUrl: './Image.component.html',
+  styleUrls: ['./Image.component.css']
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
