f8e34a2fc1a9f18d7cc7bce6ff25b736b895ea88
jadertao <jader_tao@qq.com>
2020-05-01 01:02:20 +0800

    feat(list): init list component

---
+++ projects/ng-arwes/src/lib/components/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arwes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
