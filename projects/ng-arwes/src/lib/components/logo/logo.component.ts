d66012170c33a8e05ac676fe7075f3a42104313b
jadertao <jader_tao@qq.com>
2020-05-04 23:41:56 +0800

    feat(logo): init logo component

---
+++ projects/ng-arwes/src/lib/components/logo/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arwes-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.css']
+})
+export class LogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
