f608ccb30aae7644bd3eb44975cf9e620f3f0fac
jader_tao <Jader_tao@qq.com>
2020-05-24 23:53:41 +0800

    feat(style): method getTag implement

--- projects/ng-arwes/src/lib/services/style.service.ts
+++ projects/ng-arwes/src/lib/services/style.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +8,29 @@ export class StyleService {
 
   cache: Record<string, HTMLStyleElement> = {};
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private doc: Document,
+  ) { }
+
+  getSelector(name: string): string {
+    return `head .${name}`;
+  }
 
   getTag(name: string): HTMLStyleElement {
     let tag = this.cache[name];
     if (tag) {
       return tag;
     }
+    tag = this.doc.querySelector<HTMLStyleElement>(this.getSelector(name));
+    if (tag) {
+      return tag;
+    }
+    tag = this.doc.createElement('style');
+    tag.type = 'text/css';
+    tag.className = name;
+    tag.appendChild(this.doc.createTextNode(''));
+    this.doc.head.appendChild(tag);
+    this.cache.name = tag;
+    return tag;
   }
 }
