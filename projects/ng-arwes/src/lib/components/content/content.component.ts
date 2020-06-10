95a872f82f0be5d50a1c1537b057767516eca361
jader_tao <Jader_tao@qq.com>
2020-06-10 23:43:41 +0800

    feat(content): init

---
+++ projects/ng-arwes/src/lib/components/content/content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arwes-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
