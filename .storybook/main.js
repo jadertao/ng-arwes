cebb25755290002498a15d80585429b0ac766850
jader_tao <Jader_tao@qq.com>
2020-06-12 23:51:51 +0800

    feat(addon): note panel

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes/register', '@storybook/addon-knobs/register'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes/register-panel', '@storybook/addon-knobs/register'],
 };
