d96aa9ea22fd4c9000143e74a948516e9b0caead
jader_tao <Jader_tao@qq.com>
2020-07-16 23:32:41 +0800

    feat(responsive): complete

--- projects/ng-arwes/src/lib/services/responsive/responsive.service.ts
+++ projects/ng-arwes/src/lib/services/responsive/responsive.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ThemeService } from '../theme/theme.service';
-import { fromEvent, Subject } from 'rxjs';
+import { fromEvent, Subject, Observable, of } from 'rxjs';
 import { isNode } from 'ng-arwes/tools/isNode';
+import { getDimensions } from 'ng-arwes/tools/dimension';
+import { map } from 'rxjs/operators';
 
 
-enum ResponsiveStatusType {
+export enum ResponsiveStatusType {
   small = 'small',
   medium = 'medium',
   large = 'large',
@@ -13,12 +15,11 @@ enum ResponsiveStatusType {
 
 type ResponsiveStatusItem<T extends ResponsiveStatusType> = { [index in T]: true } & { status: T };
 
-type ResponsiveStatus = ResponsiveStatusItem<ResponsiveStatusType.small>;
-
-const type: ResponsiveStatusItem<ResponsiveStatusType.small> = {
-  small: true,
-  status: ResponsiveStatusType.small
-};
+type ResponsiveStatus =
+  | ResponsiveStatusItem<ResponsiveStatusType.small>
+  | ResponsiveStatusItem<ResponsiveStatusType.medium>
+  | ResponsiveStatusItem<ResponsiveStatusType.large>
+  | ResponsiveStatusItem<ResponsiveStatusType.xlarge>;
 
 @Injectable({
   providedIn: 'root'
@@ -28,14 +29,30 @@ export class ResponsiveService {
   change: Subject<any>;
 
   constructor(
-    private theme: ThemeService
+    private themeService: ThemeService
   ) {
     this.change = new Subject<any>();
     if (!isNode) {
       fromEvent(window, 'resize', { capture: false }).subscribe(v => console.log(v));
     }
   }
-
+  get$(): Observable<ResponsiveStatus> {
+    return this.themeService.theme$.pipe(
+      map(theme => {
+        // return { status: ResponsiveStatusType.large, large: true };
+        const { width } = getDimensions();
+        const { small, medium, large } = theme.responsive;
+        if (width <= small) {
+          return { status: ResponsiveStatusType.small, small: true };
+        } else if (width <= medium) {
+          return { status: ResponsiveStatusType.medium, medium: true };
+        } else if (width <= large) {
+          return { status: ResponsiveStatusType.large, large: true };
+        }
+        return { status: ResponsiveStatusType.xlarge, xlarge: true };
+      })
+    );
+  }
   status$() {
 
   }
