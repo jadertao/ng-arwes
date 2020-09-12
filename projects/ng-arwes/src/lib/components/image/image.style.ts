b1e8abf93b3f2e90489b78701c378badbe4e44a1
jader_tao <Jader_tao@qq.com>
2020-09-12 23:32:39 +0800

    feat(image): jss

--- projects/ng-arwes/src/lib/components/image/image.style.ts
+++ projects/ng-arwes/src/lib/components/image/image.style.ts
@@ -1,8 +1,81 @@
 import { rgba } from 'polished';
 import { ArwesImageInput } from './image.component';
 import { ComponentClassFn, ComponentInstanceFn } from 'ng-arwes/tools/style';
+import { NgArwesTheme } from 'ng-arwes/types/theme.interfaces';
+
+interface NgArwesImageStyleParams {
+  theme: NgArwesTheme;
+  input: ArwesImageInput;
+}
+
 
 export const NgArwesImageStyle = {
+  root: {
+    position: 'relative',
+    display: 'block',
+    margin: ({ input, theme }: NgArwesImageStyleParams) => [0, 0, theme.margin],
+    width: '100%',
+    minHeight: 1,
+    verticalAlign: 'middle',
+
+  },
+  holder: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    transition: ({ input, theme }: NgArwesImageStyleParams) => `all ${theme.animTime}ms ease-out`,
+    minHeight: 200
+  },
+  img: {
+    display: 'block',
+    border: 'none',
+    margin: 0,
+    padding: 0,
+    width: '100%',
+    height: 'auto',
+    verticalAlign: 'top',
+    boxShadow: 'none',
+    transition: ({ input, theme }: NgArwesImageStyleParams) => `all ${theme.animTime}ms ease-in`,
+    opacity: 0
+  },
+  error: {
+    display: 'block',
+    margin: 'auto',
+    textAlign: 'center',
+    color: ({ input, theme }: NgArwesImageStyleParams) => theme.color.alert.base
+  },
+  separator: {
+    position: 'absolute',
+    top: 'auto',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    display: 'block',
+    width: '100%',
+    borderStyle: 'solid',
+    borderColor: ({ input, theme }: NgArwesImageStyleParams) =>
+      rgba(theme.color[input.layer].dark, theme.alpha / 2),
+    borderWidth: '0 0 1px',
+    transition: ({ input, theme }: NgArwesImageStyleParams) => `all ${theme.animTime}ms ease-in`
+  },
+  children: {
+    display: 'block',
+    margin: 0,
+    padding: ({ input, theme }: NgArwesImageStyleParams) => theme.padding / 2,
+    textAlign: 'center',
+    backgroundColor: ({ input, theme }: NgArwesImageStyleParams) => theme.background[input.layer].level1,
+    color: ({ input, theme }: NgArwesImageStyleParams) => theme.color[input.layer].dark,
+    transition: ({ input, theme }: NgArwesImageStyleParams) => `opacity ${theme.animTime}ms ease-in`,
+    opacity: 1
+  },
+
+  ready: {
+    '& $holder': {
+      minHeight: 'auto'
+    },
+    '& $img': {
+      opacity: 1
+    }
+  },
 
 };
 
